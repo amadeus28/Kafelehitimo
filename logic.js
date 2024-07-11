0cd5c17fe1c9c3a0772d8fab25ddf8a1efb2b924
amadeus28 <152012378+amadeus28@users.noreply.github.com>
2024-07-11 14:26:03 +0800

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', function() {
+            var defaultUsername = "123";
+            var defaultPassword = "123";
+
+            var usernameInput = document.getElementById('username');
+            var passwordInput = document.getElementById('password');
+
+            if (usernameInput && passwordInput) {
+                usernameInput.value = sessionStorage.getItem("username") || defaultUsername;
+                passwordInput.value = sessionStorage.getItem("password") || defaultPassword;
+            }
+
+            document.getElementById('loginForm').addEventListener('submit', function(event) {
+                event.preventDefault();
+                var username = usernameInput.value;
+                var password = passwordInput.value;
+                if (username === "123" && password === "123") {
+                    sessionStorage.setItem("username", username);
+                    sessionStorage.setItem("password", password);
+                    alert("Login successful!");
+                    window.location.href = "index.html"; 
+                } else {
+                    alert("Invalid username or password.");
+                }
+            });
+        });
+
+document.addEventListener("DOMContentLoaded", function() {
+            const purchaseButtons = document.querySelectorAll(".purchase-btn");
+        
+            purchaseButtons.forEach(button => {
+                button.addEventListener("click", function() {
+                    alert_click();
+                });
+            });
+        
+            function alert_click() {
+                alert("Transaction Complete.");
+            }
+        });
